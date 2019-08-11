c077eb2e16967b3f3ec0cc043c00b4b0d981cd97
fzxiao233 <fzxiao@outlook.com>
2019-08-11 11:41:18 +0800

    修复部分数据库插入问题 添加type lint
    Signed-off-by: fzxiao233 <fzxiao@outlook.com>

--- worker/cq_worker.py
+++ worker/cq_worker.py
@@ -12,13 +12,13 @@ logger = get_logger()
 
 
 class CQWorker:
-    def __init__(self, video_dict):
+    def __init__(self, video_dict: dict) -> None:
         self.video_dict = video_dict
         self.end_sub = Subscriber(('cq',))
 
     @staticmethod
-    def formatter(event_info):
-        def time_formatter():
+    def formatter(event_info: dict) -> dict:
+        def time_formatter() -> str:
             time_obj = datetime.fromtimestamp(raw_time)
             struct_time = datetime.strftime(time_obj, '%Y年%m月%d日%H:%M:%S')
             return struct_time
@@ -33,11 +33,11 @@ class CQWorker:
         return struct_event_info
 
     def txt_recorder(self, event_info: dict):
-        with open(f'{config["ddir"]}/{self.video_dict["Title"]}.txt', 'a') as f:
+        with open(f'{config["ddir"]}/{self.video_dict["Title"]}', 'a') as f:
             f.write(f"{event_info['time']}: {event_info['msg']} \n")
         logger.warning(f"{event_info['time']}: {event_info['msg']}")
 
-    async def ws_interaction(self):
+    async def ws_interaction(self) -> None:
         while True:
             try:
                 async with websockets.connect(config['cq_ws_uri']) as ws:
@@ -54,7 +54,7 @@ class CQWorker:
                 logger.exception('Ws connection was broken')
 
     @staticmethod
-    def event_filter(event_info):
+    def event_filter(event_info: dict) -> bool or dict:
         if event_info['post_type'] != 'message':
             return False
         if event_info['user_id'] != 2944950852:
@@ -62,31 +62,34 @@ class CQWorker:
             return False
         return event_info
 
-    def end_record(self):
-        video_dict = self.end_sub.do_subscribe_nowait()
-        if video_dict == self.video_dict:
-            logger.info(f'{video_dict["Title"]} Done')
+    def end_record(self) -> bool:
+        target = self.end_sub.do_subscribe_nowait()
+        if target == self.video_dict['Target']:
+            logger.info(f'{self.video_dict["Title"]} Done')
             return True
 
-    def upload_record(self):
+    def upload_record(self, ddir: str) -> None:
         pub = Publisher()
         upload_dict = {
-            'Title': self.video_dict['Title'] + '.txt',
+            'Title': self.video_dict['Title'],
+            'Target': self.video_dict['Target'],
+            'Path': f'{ddir}/{self.video_dict["Title"]}',
             'User': self.video_dict['User'],
             'Record': True
         }
         pub.do_publish(upload_dict, 'upload')
 
-    async def main(self):
+    async def main(self) -> None:
         user_config = get_user(self.video_dict['User'])
         if not user_config['record']:
             return None
         ddir = get_ddir(user_config)
-        self.video_dict['Title'] = AdjustFileName(self.video_dict['Title']).adjust(ddir)
+        self.video_dict['Title'] = AdjustFileName(self.video_dict['Title'] + '.txt').adjust(ddir)
         await self.ws_interaction()
+        self.upload_record(ddir)
 
 
-def start():
+def start() -> None:
     sub = Subscriber(('main',))
     logger.info('Cq worker is already running')
     while True:
