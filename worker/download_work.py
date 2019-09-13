b77b27a96909df5ee9b091af047456202f7fe634
fzxiao <fzxiao@Outlook.com>
2019-09-13 15:14:14 +0800

    增加熟肉留档 重构！
    Signed-off-by: fzxiao <fzxiao@Outlook.com>

--- worker/download_work.py
+++ worker/download_work.py
@@ -13,8 +13,17 @@ from tools import check_ddir_is_exist, get_ddir, get_logger, get_user, AdjustFil
 logger = get_logger()
 
 
-def downloader(link: str, title: str, dl_proxy: str, ddir: str,
-               quality: str = 'best') -> str:
+def check_downloaded(paths):
+    if isfile(paths):
+        logger.info(f'{paths} has been downloaded.')
+        return True
+    else:
+        logger.error(f'{paths} Download error')
+        raise RuntimeError
+
+
+def download_by_streamlink(link: str, title: str, dl_proxy: str, ddir: str,
+                           quality: str = 'best') -> str:
     # co = ["streamlink", "--hls-live-restart", "--loglevel", "trace", "--force"]
     co: list = ["streamlink", "--hls-live-restart", "--force"]
     filename: str = title + '.ts'
@@ -29,16 +38,62 @@ def downloader(link: str, title: str, dl_proxy: str, ddir: str,
     co.append(link)
     co.append(quality)
     subprocess.run(co)
-    if isfile(paths):
-        logger.info(f'{title} has been downloaded.')
+    if check_downloaded(paths):
         return filename
-    else:
-        logger.error(f'{title} Download error, link: {link}')
-        raise RuntimeError
+
+
+def download_by_youtube_dl(link: str, title: str, dl_proxy: str, ddir: str):
+    filename: str = title + '.ts'
+    paths: str = f'{ddir}/{filename}'
+    co: list = ['youtube-dl', '-o', paths]
+    if config['enable_proxy']:
+        co.append('--proxy')
+        co.append(f'http://{dl_proxy}')
+    co.append(link)
+    subprocess.run(co)
+    if check_downloaded(paths):
+        return filename
 
 
 def get_timestamp():
     return int(datetime.now().timestamp() * 1000)
+
+
+def download_video(video_dict, ddir):
+    if video_dict["Provide"] == 'Youtube':
+        result: str = download_by_streamlink(f"https://www.youtube.com/watch?v={video_dict['Ref']}",
+                                             video_dict['Title'],
+                                             config['proxy'], ddir, config['youtube_quality'])
+    elif video_dict["Provide"] == 'Bilibili':
+        result: str = download_by_youtube_dl(video_dict['Target'], video_dict['Title'], config['proxy'], ddir)
+    else:
+        result: str = download_by_streamlink(video_dict['Ref'], video_dict['Title'], config['proxy'], ddir)
+    return result
+
+
+def send_bot(result, user):
+    pub = Publisher()
+    data = {'Msg': f"[下载提示] {result} 已下载完成，等待上传",
+            'User': user}
+    logger.warning(data)
+    pub.do_publish(data, 'bot')
+
+
+def send_upload(video_dict, path):
+    if not config['enable_upload']:
+        return
+    pub = Publisher()
+    upload_dict = {
+        'Title': video_dict['Title'],
+        'Target': video_dict['Target'],
+        'Date': video_dict['Date'],
+        'Path': path,
+        'User': video_dict['User'],
+        'Origin_Title': video_dict['Origin_Title'],
+        'ASS': get_ass(video_dict)
+    }
+    pub.do_publish(upload_dict, 'upload')
+    pub.do_publish(video_dict['Target'], 'cq')
 
 
 def process_video(video_dict):
@@ -48,8 +103,6 @@ def process_video(video_dict):
     :return: None
     """
     user_config: dict = get_user(video_dict['User'])
-    if video_dict['Provide'] == 'Bilibili':
-        return None
     if not user_config['download']:
         return None
     ddir: str = get_ddir(user_config)
@@ -58,29 +111,10 @@ def process_video(video_dict):
     video_dict['Origin_Title'] = video_dict['Title']
     video_dict['Title'] = AdjustFileName(video_dict['Title']).adjust(ddir)
     video_dict['Start_timestamp'] = get_timestamp()
-    if video_dict["Provide"] == 'Youtube':
-        result: str = downloader(f"https://www.youtube.com/watch?v={video_dict['Ref']}", video_dict['Title'],
-                                 config['proxy'], ddir, config['youtube_quality'])
-    else:
-        result: str = downloader(video_dict['Ref'], video_dict['Title'], config['proxy'], ddir)
-    pub = Publisher()
+    video_dict['Title'] = download_video(video_dict, ddir)
     video_dict['End_timestamp'] = get_timestamp()
-    data = {'Msg': f"[下载提示] {result} 已下载完成，等待上传",
-            'User': user_config['user']}
-    logger.warning(data)
-    pub.do_publish(data, 'bot')
-    if config['enable_upload']:
-        upload_dict = {
-            'Title': result,
-            'Target': video_dict['Target'],
-            'Date': video_dict['Date'],
-            'Path': f'{ddir}/{result}',
-            'User': video_dict['User'],
-            'Origin_Title': video_dict['Origin_Title'],
-            'ASS': get_ass(video_dict)
-        }
-        pub.do_publish(upload_dict, 'upload')
-        pub.do_publish(video_dict['Target'], 'cq')
+    send_bot(video_dict['Title'], user_config['user'])
+    send_upload(video_dict, f'{ddir}/{video_dict["Title"]}')
 
 
 def get_ass(video_dict: dict) -> str:
